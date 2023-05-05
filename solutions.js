37d7aac9348402af3ad384a5571316ab5529df96
Je Min Yun <jmy@macbook-pro.myfiosgateway.com>
2023-05-05 18:22:21 -0400

    palindrome completed

--- solutions.js
+++ solutions.js
@@ -1,9 +1,26 @@
-//////////////// section1 /////////////////
-///////////////////////////////////////////
-What is the difference between a parameter and an argument?
-// A parameter is a variable that is declared in a function definition. It is a placeholder for a value that will be passed to the function when it is called. An argument is the value that is actually passed to a function when it is called.
+// //////////////// section1 /////////////////
+// ///////////////////////////////////////////
+// What is the difference between a parameter and an argument?
+// // A parameter is a variable that is declared in a function definition. It is a placeholder for a value that will be passed to the function when it is called. An argument is the value that is actually passed to a function when it is called.
 
-Within a function, what is the difference between return and console.log?
-// The main difference between return and console.log is that return returns a value from a function, while console.log simply logs a value to the console.
+// Within a function, what is the difference between return and console.log?
+// // The main difference between return and console.log is that return returns a value from a function, while console.log simply logs a value to the console.
 
 
+/////////////// section2 /////////////////
+//////////////////////////////////////////
+// Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
+function checkPalindrome(str) {
+    // Convert the string to lowercase.. just in case it starts with uppercase..
+    str = str.toLowerCase();
+
+    // Reverse the lowercased string...., array -> reverse -> string..
+    str2 = str.split("").reverse().join("");
+
+    // Compare the original string to the reversed string...
+    return str === str2
+  }
+
+  console.log(checkPalindrome("Radar"));
+  console.log(checkPalindrome("Jemin"));
+  console.log(checkPalindrome("Yun"));
