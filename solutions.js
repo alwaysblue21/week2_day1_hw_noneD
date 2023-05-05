a2e0de96405d5fc83ea292a922015a1fbede457e
Je Min Yun <jmy@macbook-pro.myfiosgateway.com>
2023-05-05 19:29:30 -0400

    sum array completed

--- solutions.js
+++ solutions.js
@@ -28,3 +28,28 @@
 //   console.log(checkPalindrome("Yun"));
 
   ////////////////// section 3 ////////////////////
+  /////////////////////////////////////////////////
+//   Write a function sumArray that takes an array as an argument. The array should contain numbers. The function should return the sum of the numbers in the array. Use a for loop within the function to iterate over the array and sum the contents.
+// array -> loop -> set a variable to receive return...
+// function sumArray(array) {
+//     // set a variable....
+//     let sum = 0;
+//     // iterate over the array using for loop
+//     for (let i = 0; i < array.length; i++) {
+//         //[i] used to access the element at index i....
+//         sum += array[i];
+//     }
+//     //return the value of sum..
+//     return sum;
+// }
+
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+
+
+
+
+
+
+
+
